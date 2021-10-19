40bf5efd0263271a55169c032d0252450c5be13f
Rafael Hernandez <rafael@koalaworkshop.com>
2021-10-18 19:59:56 -0500

    Add elements

---
+++ components/Element.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="outer relative w-48 h-32 rounded-lg shadow" :style="style">
+    <span
+      class="w-full h-full flex justify-center items-center text-5xl font-black"
+    >
+      {{ element.symbol }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    element: {},
+  },
+  computed: {
+    style() {
+      return {
+        "background-color": '#' + (this.element["cpk-hex"] ?? "dd77ff"),
+      };
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,28 @@
 <template>
-  <Tutorial />
+  <p v-if="$fetchState.pending">
+    Fetching mountains...
+    </p>
+  <p v-else-if="$fetchState.error">
+    An error occurred :(
+      </p>
+  <div v-else>
+    <h1>Nuxt Mountains</h1>
+    <div  >
+        <Element v-for="element of posts.elements" :key="element.number" :element="element"/>
+    </div>
+    <button @click="$fetch">Refresh</button>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    posts: []
+  }),
+ async fetch() {
+      this.posts = await fetch(
+        'https://run.mocky.io/v3/5b989362-1dac-416a-b2fa-e40155958025'
+      ).then(res => res.json())
+    }
+}
 </script>
